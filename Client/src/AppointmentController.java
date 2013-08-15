6f8fc0eec148204859d56b3471a3769557d5131f
Melinda Kingsun <mindikingsun@gmail.com>
2013-08-15 13:58:45 +1000

    Partial Commit

---
+++ Client/src/AppointmentController.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: mindikingsun
+ * Date: 15/08/13
+ * Time: 1:55 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AppointmentController extends  {
+}
