2e086189ebc2b41a8563ef68522b3d8fea820f25
Melinda Kingsun <mindikingsun@gmail.com>
2013-08-25 18:20:11 +1000

    Models for ServiceHours and ServiceProvider added.

---
+++ ShearNDipityLib/src/Models/ServiceHours.java
@@ -0,0 +1,85 @@
+package Models;
+
+import java.sql.Time;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: mindikingsun
+ * Date: 25/08/13
+ * Time: 5:53 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ServiceHours {
+
+    private int servId;
+    private int servHrsDay;
+    private Time servHrsStart;
+    private Time servHrsBreakStart;
+    private Time servHrsBreakEnd;
+    private Time servHrsEnd;
+
+    public ServiceHours() {
+    }
+
+    public ServiceHours(int servId, int servHrsDay) {
+        this.servId = servId;
+        this.servHrsDay = servHrsDay;
+    }
+
+    public ServiceHours(int servId, int servHrsDay, Time servHrsStart, Time servHrsBreakStart, Time servHrsBreakEnd, Time servHrsEnd) {
+        this.servId = servId;
+        this.servHrsDay = servHrsDay;
+        this.servHrsStart = servHrsStart;
+        this.servHrsBreakStart = servHrsBreakStart;
+        this.servHrsBreakEnd = servHrsBreakEnd;
+        this.servHrsEnd = servHrsEnd;
+    }
+
+    public int getServId() {
+        return servId;
+    }
+
+    public void setServId(int servId) {
+        this.servId = servId;
+    }
+
+    public int getServHrsDay() {
+        return servHrsDay;
+    }
+
+    public void setServHrsDay(int servHrsDay) {
+        this.servHrsDay = servHrsDay;
+    }
+
+    public Time getServHrsStart() {
+        return servHrsStart;
+    }
+
+    public void setServHrsStart(Time servHrsStart) {
+        this.servHrsStart = servHrsStart;
+    }
+
+    public Time getServHrsBreakStart() {
+        return servHrsBreakStart;
+    }
+
+    public void setServHrsBreakStart(Time servHrsBreakStart) {
+        this.servHrsBreakStart = servHrsBreakStart;
+    }
+
+    public Time getServHrsBreakEnd() {
+        return servHrsBreakEnd;
+    }
+
+    public void setServHrsBreakEnd(Time servHrsBreakEnd) {
+        this.servHrsBreakEnd = servHrsBreakEnd;
+    }
+
+    public Time getServHrsEnd() {
+        return servHrsEnd;
+    }
+
+    public void setServHrsEnd(Time servHrsEnd) {
+        this.servHrsEnd = servHrsEnd;
+    }
+}
---
+++ ShearNDipityLib/src/Models/ServiceProvider.java
@@ -0,0 +1,97 @@
+package Models;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: mindikingsun
+ * Date: 25/08/13
+ * Time: 5:40 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+/**
+ * ServiceProvider, extends Contact to for a service provider.  Each Service Provider must have an Initiated value.
+ * Each Service Provider requires ServiceHours, in the form of an array of ServiceHours objects.
+ */
+public class ServiceProvider extends Contact {
+
+    private int servId;
+    private String servBio;
+    private String servPortrait;
+    private String servInitiated;
+    private String serTerminated;
+    private String servIsActive;
+    private ServiceHours[] serviceHours;
+
+    public ServiceProvider() {
+        initialiseServiceHours();
+    }
+
+    public ServiceProvider(String contForename, String contSurname, String servInitiated) {
+        super(contForename, contSurname);    //To change body of overridden methods use File | Settings | File Templates.
+        this.servInitiated = servInitiated;
+        initialiseServiceHours();
+    }
+
+    public int getServId() {
+        return servId;
+    }
+
+    public void setServId(int servId) {
+        this.servId = servId;
+    }
+
+    public String getServBio() {
+        return servBio;
+    }
+
+    public void setServBio(String servBio) {
+        this.servBio = servBio;
+    }
+
+    public String getServPortrait() {
+        return servPortrait;
+    }
+
+    public void setServPortrait(String servPortrait) {
+        this.servPortrait = servPortrait;
+    }
+
+    public String getServInitiated() {
+        return servInitiated;
+    }
+
+    public void setServInitiated(String servInitiated) {
+        this.servInitiated = servInitiated;
+    }
+
+    public String getSerTerminated() {
+        return serTerminated;
+    }
+
+    public void setSerTerminated(String serTerminated) {
+        this.serTerminated = serTerminated;
+    }
+
+    public String getServIsActive() {
+        return servIsActive;
+    }
+
+    public void setServIsActive(String servIsActive) {
+        this.servIsActive = servIsActive;
+    }
+
+    private void initialiseServiceHours() {
+        for (int i = 0; i < 7; i++) {
+            serviceHours[i] = new ServiceHours();
+        }
+    }
+
+    public ServiceHours getByDay(int dayNum) {
+        return this.serviceHours[dayNum];
+    }
+
+
+    public void serByDay(int dayNum, ServiceHours servHours) {
+        this.serviceHours[dayNum] = servHours;
+    }
+}
