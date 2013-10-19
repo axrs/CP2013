26bd1c7553d8e9aba56afce0dd67db784d4459ce
Alexander Scott <xander@cadcoder.com>
2013-10-19 15:40:52 +1000

    Contact Command

---
+++ ClientView/src/client/controllers/ExistingContactWindowCommand.java
@@ -0,0 +1,15 @@
+package client.controllers;
+
+import Models.Contact;
+
+public class ExistingContactWindowCommand extends NewStage implements ICommand {
+
+    private Contact contact = null;
+
+
+
+    @Override
+    public void execute() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
