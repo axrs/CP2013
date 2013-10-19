c0cc02cfed0bf031f8a19525be79b5949a0e7bf5
Tim <timhart404@gmail.com>
2013-10-19 15:33:44 +1000

    Aboutviewcommand updated

--- ClientView/src/client/controllers/ShowAboutWindowCommand.java
+++ ClientView/src/client/controllers/ShowAboutWindowCommand.java
@@ -2,9 +2,15 @@ package client.controllers;
 
 import Models.Config;
 import client.GoogleMap;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.VPos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
@@ -30,13 +36,15 @@ public class ShowAboutWindowCommand implements ICommand {
 
     @Override
     public void execute() {
-        Stage aboutStage = new Stage();
+        final Stage aboutStage = new Stage();
         aboutStage.setTitle(title);
 
-        BorderPane borderPane = new BorderPane();
-        Label aboutText = new Label(description);
+        GridPane borderPane = new GridPane();
+        borderPane.setPadding(new Insets(5, 5, 5, 5));
+        borderPane.setHgap(5);
 
-        borderPane.setTop(aboutText);
+        borderPane.addRow(0, new Label("Who We Are:"), new Label("Where We Be: "));
+        borderPane.setStyle("-fx-background-color: #FFEFCC;");
 
         final WebView webView = new WebView();
         final WebEngine webEngine = webView.getEngine();
@@ -44,9 +52,26 @@ public class ShowAboutWindowCommand implements ICommand {
         String html = GoogleMap.getHtml(Config.getInstance().getGeoLocation(),
                 Config.getInstance().getZoom(),
                 Config.getInstance().getTitle());
-
+        System.out.println(html);
         webEngine.loadContent(html);
-        borderPane.setCenter(webView);
+        webView.setMaxSize(300, 300);
+
+        Label aboutText =new Label(description);
+
+        borderPane.add( aboutText, 0, 1);
+        borderPane.add(webView, 1, 1, 1, 4);
+        GridPane.setValignment(aboutText, VPos.TOP);
+
+        Button closeButton = new Button("I'm Done Here");
+        closeButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                aboutStage.close();
+            }
+        });
+
+        borderPane.add(closeButton, 0, 4);
+        GridPane.setValignment(closeButton, VPos.BOTTOM);
 
         aboutStage.setScene(new Scene(borderPane));
         aboutStage.show();
