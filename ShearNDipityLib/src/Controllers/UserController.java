cc9dbf60527736f105f556618d42e743e15cda59
Melinda Kingsun <mindikingsun@gmail.com>
2013-10-26 14:16:25 +1000

    Stuff

--- ShearNDipityLib/src/Controllers/UserController.java
+++ ShearNDipityLib/src/Controllers/UserController.java
@@ -1,7 +1,24 @@
 package Controllers;
 
 
+
+import java.util.Date;
+
 public class UserController {
 
-   private static UserController instance = null;
+    private static UserController instance = null;
+    private Mutex UserLocker;
+    private Date lastUpdate;
+
+    /**
+     * Singleton constructor.  Not able to be overridden
+     */
+    protected UserController() {
+        if (UserLocker == null) {
+            UserLocker = new Mutex();
+            lastUpdate = new Date();
+        }
+        if ((new Date().getTime() - lastUpdate.getTime()) > 60000) {
+        }
+    }
 }
