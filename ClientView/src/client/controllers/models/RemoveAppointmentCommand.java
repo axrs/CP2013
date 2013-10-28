175d89a78ac7069895012bc84905b027f4473f3d
Alex <Alex@SKYCATCHER.(none)>
2013-10-28 20:09:24 +1000

    Remove Appointment Command

---
+++ ClientView/src/client/controllers/models/RemoveAppointmentCommand.java
@@ -0,0 +1,40 @@
+package client.controllers.models;
+
+import client.controllers.ICommand;
+import client.scene.INotifiable;
+import dao.DAO;
+import dao.restDAO.events.Result;
+import dao.restDAO.listeners.ResultListener;
+import models.Appointment;
+
+
+public class RemoveAppointmentCommand extends ICommand {
+
+    Appointment appointment = null;
+    INotifiable source = null;
+
+    public RemoveAppointmentCommand(Appointment appointment, INotifiable source) {
+        this.source = source;
+        this.appointment = appointment;
+    }
+
+    @Override
+    public void execute() {
+        DAO.getInstance().getAppointmentDAO().remove(appointment.getTypeId(), new ResultListener() {
+            @Override
+            public void results(Result result) {
+                switch (result.getStatus()) {
+                    case 202:
+                        source.onSuccess();
+                        break;
+                    case 400:
+                        source.onError("Bad request to server.");
+                        break;
+                    case 500:
+                        source.onError("Database Error");
+                        break;
+                }
+            }
+        });
+    }
+}
